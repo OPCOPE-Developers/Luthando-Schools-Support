b060669a08432005c2efdff25e660174d15e172b
unknown <220477019@mycput.ac.za>
2022-09-28 19:48:06 +0200

    Completed Routing for the site

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePageComponent } from './home-page/home-page.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProvideSupportComponent } from './provide-support/provide-support.component';
+import { RegisterOrganisationComponent } from './register-organisation/register-organisation.component';
+import { RegisterSchoolComponent } from './register-school/register-school.component';
+import { RegisterUserComponent } from './register-user/register-user.component';
+import { ViewProfileComponent } from './view-profile/view-profile.component';
 
 const routes: Routes =[
-  {path:'home', component: HomePageComponent, pathMatch :'full'}
+  {path:'home', component: HomePageComponent, pathMatch :'full'},
+  {path:'provide-support', component: ProvideSupportComponent, pathMatch :'full'},
+  {path:'register-organisation', component: RegisterOrganisationComponent, pathMatch :'full'},
+  {path:'register-school', component: RegisterSchoolComponent, pathMatch :'full'},
+  {path:'register-user', component: RegisterUserComponent, pathMatch :'full'},
+  {path:'view-profile', component: ViewProfileComponent, pathMatch :'full'}
 ];
 
 @NgModule({
